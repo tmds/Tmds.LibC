6ad0791929275adcc994b67258e6a1c966d8a8e5
Tom Deseyn <tom.deseyn@gmail.com>
2019-01-22 11:13:29 +0100

    Add tool to generate README with list of implemented functions (#7)
    * Add tool to generate README with list of implemented functions
    
    * List each function once
    
    * Don't swallow HttpClient exceptions

---
+++ tools/GenerateDoc/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Tmds.LibC;
+
+namespace GenerateDoc
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var methodNames = new List<string>();
+            var methods = typeof(Definitions).GetMethods(BindingFlags.Static | BindingFlags.Public);
+            foreach (var method in methods)
+            {
+                // Skip property getters
+                bool isGetAccessor = method.DeclaringType.GetProperties() 
+                    .Any(prop => prop.GetGetMethod() == method);
+                if (isGetAccessor)
+                {
+                    continue;
+                }
+
+                // Skip MACRO functions
+                if (method.Name == method.Name.ToUpper())
+                {
+                    continue;
+                }
+
+                methodNames.Add(method.Name);
+            }
+
+            // Sort
+            methodNames.Sort();
+            methodNames = methodNames.Distinct().ToList();
+
+            var sb = new StringBuilder();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                foreach (var methodName in methodNames)
+                {
+                    string url = string.Empty;
+
+                    string man2url = $"http://man7.org/linux/man-pages/man2/{methodName}.2.html";
+                    string man3url = $"http://man7.org/linux/man-pages/man3/{methodName}.3.html";
+
+                    if (await IsValidManPage(httpClient, man3url))
+                    {
+                        url = man3url;
+                    }
+                    else if (await IsValidManPage(httpClient, man2url))
+                    {
+                        url = man2url;
+                    }
+
+                    if (string.IsNullOrEmpty(url))
+                    {
+                        sb.Append($"{methodName}, ");
+                    }
+                    else
+                    {
+                        sb.Append($"[{methodName}]({url}), ");
+                    }
+                }
+            }
+
+            string readme = File.ReadAllText("../../README.md.in");
+
+            readme = readme.Replace("##FUNCTIONS##", sb.ToString());
+
+            File.WriteAllText("../../README.md", readme);
+        }
+
+        private async static Task<bool> IsValidManPage(HttpClient httpClient, string url)
+        {
+            using (var response = await httpClient.GetAsync(url))
+            {
+                return response.StatusCode == HttpStatusCode.OK;
+            }
+        }
+    }
+}
