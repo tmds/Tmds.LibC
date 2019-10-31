50c3d3aadda963b8780e6c1e01107453050c1118
Nicolas Portmann <ndportmann@gmail.com>
2019-10-31 20:16:23 +0100

    Fix typo

--- src/Sources/linux.specific/mman.off64.cs
+++ src/Sources/linux.specific/mman.off64.cs
@@ -6,6 +6,6 @@ namespace Tmds.Linux
     public unsafe static partial class LibC
     {
         [DllImport(libc, SetLastError = true)]
-        public static extern void* mmap(void* addr, size_t length, int prot, int flags, int fd, off_t ofset);
+        public static extern void* mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset);
     }
 }
