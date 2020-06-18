4d9608f815b19faa5ba9efb32231e8241225ba9c
Tom Deseyn <tom.deseyn@gmail.com>
2020-06-18 05:00:25 +0200

    ioctl: keep signedness of request (#58)

--- src/Sources/linux.specific/ioctl.sizet.cs
+++ src/Sources/linux.specific/ioctl.sizet.cs
@@ -6,13 +6,13 @@ namespace Tmds.Linux
     public static unsafe partial class LibC
     {
         public static int ioctl(int fd, int request)
-            => _ioctl(fd, unchecked((uint)request), 0);
+            => _ioctl(fd, request, 0);
 
         public static int ioctl(int fd, int request, int arg)
-            => _ioctl(fd, unchecked((uint)request), arg);
+            => _ioctl(fd, request, arg);
 
         public static int ioctl(int fd, int request, void* arg)
-            => _ioctl(fd, unchecked((uint)request), arg);
+            => _ioctl(fd, request, arg);
 
         [DllImport(libc, SetLastError = true, EntryPoint = "ioctl")]
         private static extern int _ioctl(int fd, syscall_arg request, syscall_arg arg);
